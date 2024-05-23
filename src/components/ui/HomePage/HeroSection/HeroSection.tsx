3d6e79c6907673e4e132d5240ccdea2e7ea491f8
JannatunNimeNishat <nishatnime100@gmail.com>
2024-05-23 13:13:44 +0600

    primary button component added

--- src/components/ui/HomePage/HeroSection/HeroSection.tsx
+++ src/components/ui/HomePage/HeroSection/HeroSection.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import assets from "@/assets";
 import Link from "next/link";
+import PrimaryBtn from "../../Button/PrimaryBtn";
 const HeroSection = () => {
   return (
     <div className="py-8 w-11/12 mx-auto flex items-center justify-center">
@@ -18,12 +19,13 @@ const HeroSection = () => {
           </div>
           <div className="mt-10">
             <Link
-              href="/bloodDonors"
-              className="rounded relative inline-flex group items-center justify-center px-3.5 py-2 m-1 cursor-pointer border-b-4 border-l-2 active:border-red-600 active:shadow-none shadow-lg bg-gradient-to-tr from-red-600 to-red-500 border-red-700 text-white"
+            href="/bloodDonors"
             >
-              <span className="absolute w-0 h-0 transition-all duration-300 ease-out bg-white rounded-full group-hover:w-32 group-hover:h-32 opacity-10"></span>
-              <span className="relative">Connect with Donors</span>
+                <PrimaryBtn>
+                Connect with Donors
+                </PrimaryBtn>
             </Link>
+            
           </div>
         </div>
         <div className=" col-span-3">
