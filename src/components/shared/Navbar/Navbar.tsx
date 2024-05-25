90d184aefd70f6c87cef843d207d582b560cd017
JannatunNimeNishat <nishatnime100@gmail.com>
2024-05-25 18:04:14 +0600

    responsive nav bar added

--- src/components/shared/Navbar/Navbar.tsx
+++ src/components/shared/Navbar/Navbar.tsx
@@ -1,94 +1,185 @@
 "use client";
 
-
 import dynamic from "next/dynamic";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
-
+import { usePathname, useRouter } from "next/navigation";
+import { useState } from "react";
+import { MenuOutlined, CloseOutlined } from "@ant-design/icons";
+import { getUserInfo, removeUser } from "@/services/actions/auth.services";
 const Navbar = () => {
   const AuthButton = dynamic(
     () => import("@/components/ui/AuthButton/AuthButton"),
     { ssr: false }
   );
   const pathName = usePathname();
+  const [smNavbarOptions, setSmNavbarOptions] = useState(false);
+  const userInfo = getUserInfo();
+  const router = useRouter();
   return (
-    <div className="flex items-center gap-16  w-full mx-auto shadow-lg">
-      <div className="bg-red-500 w-4/12 text-left flex justify-end py-3 ">
-        <Link href={'/'} className="text-4xl font-semibold text-white px-3 cursor-pointer">BloodBond</Link>
-      </div>
-      <div className="flex items-center w-8/12  gap-3 py-3">
-        <ul className="flex items-center gap-5 ">
+    <>
+      {/* sm nav */}
+      <div className=" flex lg:hidden justify-between items-center mx-4 py-[30px] ">
+        <figure>
           <Link
-            className={`${pathName === "/" ? "text-red-500" : ""}
-          hover:text-red-500 duration-150 font-medium
-          `}
             href={"/"}
+            className="text-2xl font-semibold text-red-500 px-2 cursor-pointer"
+          >
+            BloodBond
+          </Link>
+        </figure>
+
+        <figure
+          className={`duration-500  cursor-pointer`}
+          onClick={() => setSmNavbarOptions(true)}
+        >
+          <MenuOutlined className="text-3xl" />
+        </figure>
+      </div>
+      {/* sm nav bar options */}
+      <div
+        className={` fixed  top-0 right-0 z-10 w-full h-full bg-white  ${
+          smNavbarOptions ? "translate-x-0 z-10" : "translate-x-full z-10"
+        } duration-300  `}
+      >
+        <div className="flex justify-between items-center mx-4 py-[24px] font-customFontGroteskRegular">
+          <Link
+            href={"/"}
+            onClick={() => setSmNavbarOptions(false)}
+            className="text-2xl font-semibold text-red-500 px-2 cursor-pointer"
+          >
+            BloodBond
+          </Link>
+
+          <figure
+            className={` duration-500  cursor-pointer`}
+            onClick={() => setSmNavbarOptions(false)}
+          >
+            <CloseOutlined className="text-3xl" />
+          </figure>
+        </div>
+        <hr />
+        <div className="flex flex-col gap-[24px] items-end mt-[16px] px-[18px] ">
+          <Link
+            onClick={() => setSmNavbarOptions(false)}
+            href={"/"}
+            className={`${pathName === '/' ? "text-red-500" : "text-[#0F172A] "}
+            text-[18px] font-medium leading-[26px]
+            `}
+    
           >
             Home
           </Link>
           <Link
-            className={`${pathName === "/aboutUs" ? "text-red-500" : ""}
-          hover:text-red-500 duration-150 font-medium
-          `}
+            onClick={() => setSmNavbarOptions(false)}
+            href={`/dashboard/${userInfo?.role}`}
+            className="text-[#0F172A] text-[18px] font-medium leading-[26px]"
+          >
+            Dashboard
+          </Link>
+          <Link
+            onClick={() => setSmNavbarOptions(false)}
             href={"/aboutUs"}
+            className={`${pathName === '/aboutUs' ? "text-red-500" : "text-[#0F172A] "}
+            text-[18px] font-medium leading-[26px]
+            `}
+            
           >
             About Us
           </Link>
+
           <Link
-            className={`${pathName === "/bloodDonors" ? "text-red-500" : ""}
-          hover:text-red-500 duration-150 font-medium
-          `}
             href={"/bloodDonors"}
+            className={`${pathName === '/bloodDonors' ? "text-red-500" : "text-[#0F172A] "}
+            text-[18px] font-medium leading-[26px]
+            `}
+            
+            onClick={() => setSmNavbarOptions(false)}
           >
             Blood Donors
           </Link>
-        </ul>
-      <div className="cursor-pointer  ">
-        <AuthButton />
+          <Link
+            href={"/contact"}
+            className={`${pathName === '/contact' ? "text-red-500" : "text-[#0F172A] "}
+            text-[18px] font-medium leading-[26px]
+            `}
+           
+            onClick={() => setSmNavbarOptions(false)}
+          >
+            Contact
+          </Link>
+          {/* login/logout */}
+          {
+            userInfo ? 
+            <button
+            className="text-[#0F172A] text-[18px] font-medium leading-[26px]"
+            onClick={() => {
+              removeUser();
+              router.refresh();
+              router.push("/");
+              setSmNavbarOptions(false);
+            }}
+          >
+            Logout
+          </button>
+          :
+          <button
+            className="text-[#0F172A] text-[18px] font-medium leading-[26px]"
+            onClick={() => {
+              router.push("/login");
+              setSmNavbarOptions(false);
+            }}
+          >
+            Login
+          </button>
+          }
+        </div>
       </div>
+      {/* lg navbar */}
+      <div className="hidden lg:flex items-center gap-16  w-full mx-auto shadow-lg">
+        <div className="bg-red-500 w-4/12 text-left flex justify-end py-3 ">
+          <Link
+            href={"/"}
+            className="text-4xl font-semibold text-white px-3 cursor-pointer"
+          >
+            BloodBond
+          </Link>
+        </div>
+        <div className="flex items-center w-8/12  gap-3 py-3">
+          <ul className="flex items-center gap-5 ">
+            <Link
+              className={`${pathName === "/" ? "text-red-500" : ""}
+          hover:text-red-500 duration-150 font-medium
+          `}
+              href={"/"}
+            >
+              Home
+            </Link>
+            <Link
+              className={`${pathName === "/aboutUs" ? "text-red-500" : ""}
+          hover:text-red-500 duration-150 font-medium
+          `}
+              href={"/aboutUs"}
+            >
+              About Us
+            </Link>
+            <Link
+              className={`${pathName === "/bloodDonors" ? "text-red-500" : ""}
+          hover:text-red-500 duration-150 font-medium
+          `}
+              href={"/bloodDonors"}
+            >
+              Blood Donors
+            </Link>
+          </ul>
+          <div className="cursor-pointer  ">
+            <AuthButton />
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
 export default Navbar;
 
 
-/*
- <div className="flex items-center justify-between py-5 w-11/12 mx-auto">
-      <figure className="border">
-        <Link href={'/'} className="text-4xl font-semibold text-red-500 cursor-pointer">BloodBond</Link>
-      </figure>
-      <div>
-        <ul className="flex items-center gap-5 border">
-          <Link
-            className={`${pathName === "/" ? "text-red-500" : ""}
-          hover:text-red-500 duration-150 font-medium
-          `}
-            href={"/"}
-          >
-            Home
-          </Link>
-          <Link
-            className={`${pathName === "/aboutUs" ? "text-red-500" : ""}
-          hover:text-red-500 duration-150 font-medium
-          `}
-            href={"/aboutUs"}
-          >
-            About Us
-          </Link>
-          <Link
-            className={`${pathName === "/bloodDonors" ? "text-red-500" : ""}
-          hover:text-red-500 duration-150 font-medium
-          `}
-            href={"/bloodDonors"}
-          >
-            Blood Donors
-          </Link>
-        </ul>
-      </div>
-      <div className="cursor-pointer  border">
-        <AuthButton />
-      </div>
-    </div>
-*/
